098fceaf1858992db7048c400e0f1a19662865c7
apdul <shrmsdn2067@gmail.com>
2022-11-17 23:00:16 +0900

    FEAT: 투표하기 API 실행 시 Request Body를 받을 VoteValueRequestDto 생성 및 구현
    투표하기 API 실행 시 Request Body를 받을
    VoteValueRequestDto 생성 및 구현

--- src/main/java/com/dantum/daou/voteValue/VoteValueRepository.java
+++ src/main/java/com/dantum/daou/voteValue/VoteValueRepository.java
@@ -1,9 +1,10 @@
-package com.dantum.daou.vote;
+package com.dantum.daou.voteValue;
 
+import com.dantum.daou.vote.Vote;
+import com.dantum.daou.voteValue.VoteValue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface VoteValueRepository extends JpaRepository<VoteValue, Long> {
 
