87a628a2caf71020e1641c2c2c6dfe443545f277
apdul <shrmsdn2067@gmail.com>
2022-11-17 23:16:14 +0900

    FEAT: 투표 결과 조회 API의 URL과 매핑되는 컨트롤러 생성 및 구현
    투표 결과 조회 API의 URL과 매핑되는 컨트롤러 생성 및 구현

--- src/main/java/com/dantum/daou/voteValue/VoteValueController.java
+++ src/main/java/com/dantum/daou/voteValue/VoteValueController.java
@@ -2,9 +2,7 @@ package com.dantum.daou.voteValue;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,5 +12,10 @@ public class VoteValueController {
     @PostMapping("/votes/values")
     public ResponseEntity<Object> createVoteValue(@RequestBody VoteValueRequestDto requestDto) {
         return voteValueService.createVoteValue(requestDto);
+    }
+
+    @GetMapping("/votes/values/{voteIdx}")
+    public ResponseEntity<Object> getVoteResult(@PathVariable Long voteIdx) {
+        return voteValueService.getVoteResult(voteIdx);
     }
 }
