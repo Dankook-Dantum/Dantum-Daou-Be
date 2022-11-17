6854b4091db6210a36ffe19fe51f929b0a0146ca
apdul <shrmsdn2067@gmail.com>
2022-11-17 23:33:34 +0900

    FIX: 투표 상세 조회 API 시 응답할 데이터를 받을 VoteResponseDto 생성 및 구현
    투표 상세 조회 API 시 응답할 데이터를 받을 VoteResponseDto
    생성 및 구현

--- src/main/java/com/dantum/daou/vote/VoteResponseDto.java
+++ src/main/java/com/dantum/daou/vote/VoteResponseDto.java
@@ -2,20 +2,19 @@ package com.dantum.daou.vote;
 
 
 import com.dantum.daou.user.User;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class VoteResponseDto {
-
     private Long voteIdx;
     private String title;
-
     private String name;
-
     private LocalDateTime createdAt;
 
+    @Builder
     public VoteResponseDto(Vote entity){
         this.voteIdx = entity.getVoteIdx();         // 투표 idx
         this.title = entity.getTitle();             // 투표 명(내용)
