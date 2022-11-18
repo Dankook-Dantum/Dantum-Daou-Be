61576a028044e8b5a228f4810c66c64a175244d5
apdul <shrmsdn2067@gmail.com>
2022-11-19 04:06:27 +0900

    FEAT: 회원 정보 수정 API의 URL과 매핑되는 컨트롤러 생성 및 구현
    회원 정보 수정 API의 URL과 매핑되는 컨트롤러 생성 및 구현

--- src/main/java/com/dantum/daou/user/UserController.java
+++ src/main/java/com/dantum/daou/user/UserController.java
@@ -11,19 +11,25 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("users")
 public class UserController {
     private final UserService userService;
 
     @ApiOperation(value="회원정보 조회")
-    @GetMapping("/users/{userIdx}")
+    @GetMapping("/{userIdx}")
     public ResponseEntity<Object> getUser(@PathVariable("userIdx") Long userIdx) {
         return userService.getUser(userIdx);
     }
 
     @ApiOperation(value="회원정보 상세 조회")
-    @GetMapping("/users/detail/{userIdx}")
+    @GetMapping("/detail/{userIdx}")
     public ResponseEntity<Object> getDetailUser(@PathVariable("userIdx") Long userIdx) {
         return userService.getUserDetail(userIdx);
+    }
+
+    @PatchMapping("/{userIdx}")
+    public ResponseEntity<Object> updateUser(@PathVariable("userIdx") Long userIdx, @RequestBody UserRequestDto requestDto) {
+        return userService.updateUser(userIdx, requestDto);
     }
 
 }
