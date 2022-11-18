3fdf52415b4ee23490bf61a645e9d33234721d41
apdul <shrmsdn2067@gmail.com>
2022-11-18 23:25:07 +0900

    FEAT: SWAGGER 구현을 위한 어노테이션 추가
    SWAGGER 구현을 위한 어노테이션 추가

--- src/main/java/com/dantum/daou/user/UserController.java
+++ src/main/java/com/dantum/daou/user/UserController.java
@@ -1,6 +1,7 @@
 package com.dantum.daou.user;
 
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 
@@ -13,11 +14,13 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    @ApiOperation(value="회원정보 조회")
     @GetMapping("/users/{userIdx}")
     public ResponseEntity<Object> getUser(@PathVariable("userIdx") Long userIdx) {
         return userService.getUser(userIdx);
     }
 
+    @ApiOperation(value="회원정보 상세 조회")
     @GetMapping("/users/detail/{userIdx}")
     public ResponseEntity<Object> getDetailUser(@PathVariable("userIdx") Long userIdx) {
         return userService.getUserDetail(userIdx);
