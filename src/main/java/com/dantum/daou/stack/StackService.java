35ed7cc39c324423e315fdfae1bd3823fe7a7524
apdul <shrmsdn2067@gmail.com>
2022-11-19 11:15:52 +0900

    FEAT: 스택 추가 API, 스택 목록 조회 API 실행 시 동작할 서비스들 추가
    스택 추가 API, 스택 목록 조회 API 실행 시 동작할 서비스들 추가

---
+++ src/main/java/com/dantum/daou/stack/StackService.java
@@ -0,0 +1,64 @@
+package com.dantum.daou.stack;
+
+import com.dantum.daou.exception.DuplicateException;
+import com.dantum.daou.user.User;
+import com.dantum.daou.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class StackService {
+    private final StackRepository stackRepository;
+    private final UserService userService;
+
+    @Transactional
+    public List<Stack> findByUser(Long userIdx) {
+        User user = userService.findById(userIdx);
+
+        return stackRepository.findAllByUser(user);
+    }
+
+    @Transactional
+    public ResponseEntity<Object> getStack(Long userIdx) {
+        List<Stack> stackList = findByUser(userIdx);
+        List<StackDto> stackDtoList = stackList.stream()
+                .map(stack ->  {
+                    return StackDto.builder()
+                            .stack(stack.getStack()).build();
+                }).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(stackDtoList);
+    }
+
+    @Transactional
+    public ResponseEntity<Object> createStack(Long userIdx, StackDto stackDto) {
+        boolean isExist = stackRepository.existsByStack(stackDto.getStack());
+
+        if (isExist) {
+            throw new DuplicateException("Stack", "stack", stackDto.getStack());
+
+        }
+
+        User user = userService.findById(userIdx);
+        StackCreateDto stackCreateDto = StackCreateDto.builder()
+                .stack(stackDto.getStack())
+                .user(user).build();
+        Stack stack = stackCreateDto.toEntity();
+
+        stackRepository.save(stack);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Create Success");
+    }
+
+/*    @Transactional
+    public ResponseEntity<Object> updateStack(Long userIdx, StackDto stackDto) {
+
+    }*/
+}
