be4e02d6485666c7b977dfa23add5397bf74591a
apdul <shrmsdn2067@gmail.com>
2022-11-19 11:14:38 +0900

    FEAT: 스택 추가 API, 스택 목록 조회 API에 쓰일 쿼리 메소드 추가
    스택 추가 API, 스택 목록 조회 API에 쓰일 쿼리 메소드 추가

--- src/main/java/com/dantum/daou/stack/StackRepository.java
+++ src/main/java/com/dantum/daou/stack/StackRepository.java
@@ -1,6 +1,11 @@
 package com.dantum.daou.stack;
 
+import com.dantum.daou.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StackRepository extends JpaRepository<Stack, Long> {
+    List<Stack> findAllByUser(User user);
+    Boolean existsByStack(String stack);
 }
