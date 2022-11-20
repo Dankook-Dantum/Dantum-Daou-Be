51dab1bd297c7a39552e872f320679842b7f5e16
apdul <shrmsdn2067@gmail.com>
2022-11-20 22:03:39 +0900

    FIX: 유저 정보 수정 시 암호화 관련 오류 수정
    유저 정보 수정 시 암호화 관련 오류 수정

--- src/main/java/com/dantum/daou/user/UserUpdateDto.java
+++ src/main/java/com/dantum/daou/user/UserUpdateDto.java
@@ -21,15 +21,15 @@ public class UserUpdateDto {
 
     @Builder
     public UserUpdateDto(UserRequestDto requestDto, AesEncryptUtil aesEncryptUtil) throws Exception {
-        this.name = aesEncryptUtil.encryptAES256(requestDto.getName());
-        this.profileUrl = requestDto.getProfileUrl();
-        this.email = aesEncryptUtil.encryptAES256(requestDto.getEmail());
-        this.password = aesEncryptUtil.encryptAES256(requestDto.getPassword());
-        this.phone = requestDto.getPhone();
-        this.position = requestDto.getPosition();
-        this.office = requestDto.getOffice();
-        this.officeNumber = requestDto.getOfficeNumber();
-        this.introduce = requestDto.getIntroduce();
-        this.mbti = requestDto.getMbti();
+        if (requestDto.getName() != null) this.name = aesEncryptUtil.encryptAES256(requestDto.getName());
+        if (requestDto.getProfileUrl() != null) this.profileUrl = requestDto.getProfileUrl();
+        if (requestDto.getEmail() != null) this.email = aesEncryptUtil.encryptAES256(requestDto.getEmail());
+        if (requestDto.getPassword() != null) this.password = aesEncryptUtil.encryptAES256(requestDto.getPassword());
+        if (requestDto.getPhone() != null) this.phone = requestDto.getPhone();
+        if (requestDto.getPosition() != null) this.position = requestDto.getPosition();
+        if (requestDto.getOffice() != null) this.office = requestDto.getOffice();
+        if (requestDto.getOfficeNumber() != null) this.officeNumber = requestDto.getOfficeNumber();
+        if (requestDto.getIntroduce() != null) this.introduce = requestDto.getIntroduce();
+        if (requestDto.getMbti() != null) this.mbti = requestDto.getMbti();
     }
 }
