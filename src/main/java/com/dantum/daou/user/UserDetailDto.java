c07f796b4d7cafc71e591d4fc6d2a1c9486bf7ee
apdul <shrmsdn2067@gmail.com>
2022-11-20 16:34:30 +0900

    REFACTOR: 프론트 요청에 따른 회원정보 상세 조회 API 응답 형태 변경
    프론트 요청에 따른 회원정보 상세 조회 API 응답 형태 변경

--- src/main/java/com/dantum/daou/user/UserDetailDto.java
+++ src/main/java/com/dantum/daou/user/UserDetailDto.java
@@ -1,13 +1,10 @@
 package com.dantum.daou.user;
 
-import com.dantum.daou.stack.Stack;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Getter
 @NoArgsConstructor
@@ -15,13 +12,19 @@ public class UserDetailDto {
     private String office;
     private String introduce;
     private String mbti;
-    private List<Stack> stacks = new ArrayList<>();
+    private String stacks;
 
     @Builder
     public UserDetailDto(User user) {
+        StringBuilder stringBuilder = new StringBuilder();
+        user.getStacks().stream()
+                .forEach(stack -> {
+                    stringBuilder.append(stack.getStack()).append(" / ");
+                });
+        String stackReulst = stringBuilder.toString();
         this.office = user.getOffice();
         this.introduce = user.getIntroduce();
         this.mbti = user.getMbti();
-        this.stacks = user.getStacks();
+        this.stacks = stackReulst.substring(0, stackReulst.length() - 3);
     }
 }
